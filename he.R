5ecdb4713739c7edc9135f6fee63ade8f1c32cd4
Sam Clifford <sj.clifford@gmail.com>
2020-09-01 11:05:56 +0100

    using rriskDistributions

--- he.R
+++ he.R
@@ -1,10 +1,11 @@
-{gamma.parms.from.quantiles(
+{get.gamma.par(
   q = c(0.8, 3),
-  p = c(0.025, 0.975))} -> delay_parms 
+  p = c(0.025, 0.975),
+  plot = F, show.output = F)} -> delay_parms 
 
 HE <- data.frame(p = seq(1e-5, 1 - 1e-5, length.out = 101)) %>%
   mutate(delay =        
            qgamma(p, 
-                  shape = delay_parms$shape, 
-                  rate  = delay_parms$rate)) %>%
+                  shape = delay_parms[["shape"]], 
+                  rate  = delay_parms[["rate"]])) %>%
   add_row(p = 0, delay = 0, .before = 1)
