806dbe2de70a57eda54cca2552478c6c9f856ac0
Sam Clifford <sj.clifford@gmail.com>
2020-09-09 13:48:07 +0100

    table of delay statistics

--- additional_figures.R
+++ additional_figures.R
@@ -1,4 +1,4 @@
-X <- ls(pattern = "^result_") %>%
+X <- ls(pattern = "^P") %>%
   set_names(.,.) %>%
   map(get) %>%
   map_df(~data.frame(delay_to_gamma(.x)) %>%
@@ -25,7 +25,7 @@ Table_1a_summaries <-
   unnest_wider(Q) %>%
   unnest_wider(data)
 
-delay_pred <- data.frame(x = seq(0, 4, by = 0.01))
+delay_pred <- data.frame(x = seq(0, 10, by = 0.01))
 
 source_labeller <- function(x){
   sub(pattern = "result_delay_", replacement = "", x = x) %>%
@@ -56,70 +56,29 @@ Table_1a_summaries %>%
 ## total
 
 
+
 delay_total <- data.frame(sim = 1:10000) %>%
-  mutate(index_result_delay = 
+  mutate(Testing = 
            time_to_event(n = n(),
                          mean = P_r[["mean"]],
                          var  = P_r[["var"]])) %>% 
   #sample contact info delay
-  mutate(contact_info_delay = 
+  mutate(Sourcing = 
            time_to_event(n = n(),
                          mean = P_c[["mean"]],
                          var  = P_c[["var"]])) %>% 
   #sample tracing delay
-  mutate(tracing_delay      =
+  mutate(Tracing      =
            time_to_event(n = n(),
                          mean = P_t[["mean"]],
                          var  = P_t[["var"]])) %>%
-  mutate(total = index_result_delay + contact_info_delay + tracing_delay)
+  mutate(Total = Testing + Sourcing + Tracing)
 
 total_density <- 
-  pull(delay_total, total) %>%
+  pull(delay_total, Total) %>%
   {density(log(.))} %>%
   {data.frame(x = exp(.$x),
-              y = .$y, variable = "Total")}
-
-total_quantiles <- delay_total %>%
-  select(total) %>%
-  mutate(variable = "Total") %>%
-  nest(data = total) %>%
-  mutate(Q = map(data, ~quantile(.x$total, probs) %>% set_names(., probs))) %>%
-  unnest_wider(Q) %>%
-  mutate(mean = map_dbl(data, ~mean(.x$total)),
-         var  = map_dbl(data, ~var(.x$total)))
-
-delay_distributions <- 
-  list(
-    `Testing` = index_result_delay,
-    `Sourcing`= getting_contact_info,
-    `Tracing` = tracing_delay) %>%
-  map_df(~data.frame(delay_to_gamma(.x)) %>%
-           mutate(n = nrow(.x)) %>%
-           bind_cols(mv2gamma(mean = .$mean, var = .$var)),
-         .id = "variable")  %>%
-  mutate(variable = fct_inorder(factor(variable))) %>%
-  nest(data = c(scale, shape, rate)) %>%
-  mutate(Delay = map(data, ~mutate(delay_pred,
-                                   y = dgamma(x = x, 
-                                              shape = .x$shape,
-                                              rate  = .x$rate)))) %>%
-  unnest(Delay) %>%
-  bind_rows(total_density) %>%
-  mutate(variable = fct_inorder(factor(variable)))
-
-delay_quantiles <- 
-  delay_distributions %>% 
-  filter(x == 0,
-         variable != "Total") %>%
-  mutate(Q = map(data, 
-                 ~qgamma(p = probs, 
-                         shape = .x$shape,
-                         rate = .x$rate) %>%
-                   set_names(., probs)) 
-  ) %>%
-  unnest_wider(Q) %>%
-  bind_rows(total_quantiles) %>%
-  mutate(variable = fct_inorder(factor(variable)))
+              y = .$y, Event = "Total")}
 
 
 delay_histograms <- 
@@ -127,10 +86,10 @@ delay_histograms <-
     `Testing` = index_result_delay,
     `Sourcing`= getting_contact_info,
     `Tracing` = tracing_delay) %>%
-  bind_rows(.id = "variable")  %>%
-  mutate(variable = fct_inorder(factor(variable))) %>%
-  count(variable, t) %>%
-  group_by(variable) %>%
+  bind_rows(.id = "Event")  %>%
+  mutate(Event = fct_inorder(factor(Event))) %>%
+  count(Event, t) %>%
+  group_by(Event) %>%
   mutate(y = n/sum(n)) %>%
   rename(x = t)
 
@@ -142,30 +101,47 @@ delay_labeller <- function(x){
              "Total"                       = "Total")
 }
 
+delay_distributions <-
+  list(
+    Testing  = P_r,
+    Sourcing = P_c,
+    Tracing  = P_t) %>%
+  map(~mv2gamma(.x$mean, .x$var)) %>%
+  map_df(.x = .,
+         ~mutate(delay_pred,
+                 y = dgamma(x = x,
+                            shape = .x$shape,
+                            rate  = .x$rate)),
+         .id = "Event") %>%
+  bind_rows(total_density) %>%
+  mutate(Event = fct_inorder(factor(Event)))
+
 delay_plot <- ggplot(data = delay_distributions, aes(x=x, y=y)) + 
   geom_col(data = delay_histograms,
            fill = lshtm_greens[2],
            color = NA, width = 1) +
   geom_line(color = lshtm_greens[1]) +
-  geom_segment(data = delay_quantiles,
-               aes(x = `0.025`, xend = `0.975`,
+  geom_segment(data = delay_summary,
+               aes(x = `2.5%`, xend = `97.5%`,
                    y = 0, yend = 0),
                color = lshtm_greens[1],
                size = 1) +
-  geom_point(data = delay_quantiles,
-             aes(x = `0.5`, y = 0),
+  geom_point(data = delay_summary,
+             aes(x = `50%`, y = 0),
              pch = 21,
              fill = "white",
              color = lshtm_greens[1],
              size = 1) +
-  facet_wrap( ~variable,
+  facet_wrap( ~Event,
               nrow = 1,
-              labeller = labeller(variable = delay_labeller)) +
-  theme_bw() +
+              labeller = labeller(Event = delay_labeller)) +
+  theme_minimal() + 
+  theme(panel.border=element_rect(fill=NA)) +
   xlab("Time to completion (days)") +
   ylab("Density") +
-  coord_cartesian(ylim = c(0, 1.5),
-                  xlim = c(0, 6))
+  coord_cartesian(ylim = c(0, 1)) +
+  scale_x_continuous(limits = c(0,12),
+                     breaks = pretty_breaks(4))
 
 save_plot(delay_plot, 
           prefix = "delays",
