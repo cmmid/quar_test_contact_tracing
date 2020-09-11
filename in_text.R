f292c9b2c61992a0f34ef715005ec60801691c98
Sam Clifford <sj.clifford@gmail.com>
2020-09-11 14:25:07 +0100

    additional sensitivity numbers

--- in_text.R
+++ in_text.R
@@ -15,7 +15,16 @@ results_sum_results_main_subset <- read_results("sum_results_main_subset")
 results_sum_results_main_subset %>%
     filter(yvar == "infectivity_pre",
            type == "all",
-           second_test_t %in% c(0, 7, 14)) # select on quar_dur
+           quar_dur %in% c(0, 7, 14)) # select on quar_dur rather than time_since_exp
 
 ## changing TTI delays
 
+results_delay_scaling_sensivity <- read_results("delay_scaling_sensivity")
+
+results_delay_scaling_sensivity %>%
+    filter(yvar == "infectivity_averted",
+           type == "all",
+           index_test_delay == 2,
+           quar_dur %in% c(0, 7, 14)) %>%
+    select(stringency, delay_scaling, quar_dur, contains("%")) %>%
+    mutate_at(.vars = vars(contains("%")), .funs = percent_format(accuracy = 1))
